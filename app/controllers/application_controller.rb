f441bfd2fe95b495315ffc25da7cb4735c00ed92
ya.jeks <ya.jeks@yandex.ru>
2013-02-01 15:29:19 +0400

    User authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate_user
+
+  private
+    def authenticate_user
+      @current_user = User.authenticate(params[:user][:login], params[:user][:password])
+    end
 end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,83 +1,50 @@
 class VideosController < ApplicationController
-  # GET /videos
-  # GET /videos.json
   def index
     @videos = Video.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @videos }
-    end
+    render json: @videos
   end
 
-  # GET /videos/1
-  # GET /videos/1.json
   def show
     @video = Video.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @video }
-    end
+    render json: @video
   end
 
-  # GET /videos/new
-  # GET /videos/new.json
   def new
     @video = Video.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @video }
-    end
+    render json: @video
   end
 
-  # GET /videos/1/edit
   def edit
     @video = Video.find(params[:id])
   end
 
-  # POST /videos
-  # POST /videos.json
   def create
     @video = Video.new(params[:video])
 
-    respond_to do |format|
-      if @video.save
-        format.html { redirect_to @video, notice: 'Video was successfully created.' }
-        format.json { render json: @video, status: :created, location: @video }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @video.errors, status: :unprocessable_entity }
-      end
+    if @video.save
+      render json: @video, status: :created, location: @video
+    else
+      render json: @video.errors, status: :unprocessable_entity
     end
   end
 
-  # PUT /videos/1
-  # PUT /videos/1.json
   def update
     @video = Video.find(params[:id])
 
-    respond_to do |format|
-      if @video.update_attributes(params[:video])
-        format.html { redirect_to @video, notice: 'Video was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @video.errors, status: :unprocessable_entity }
-      end
+    if @video.update_attributes(params[:video])
+      head :no_content
+    else
+      render json: @video.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /videos/1
-  # DELETE /videos/1.json
   def destroy
     @video = Video.find(params[:id])
     @video.destroy
 
-    respond_to do |format|
-      format.html { redirect_to videos_url }
-      format.json { head :no_content }
-    end
+    head :no_content
   end
 end
